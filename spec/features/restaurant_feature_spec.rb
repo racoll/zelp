2fdac10bae5c0998760c3dc41d798445161223f1
robincollins <racollins1@me.com>
2017-10-18 15:24:21 +0100

    solved mega bug due to uppercase projects when starting tests and installing rspec

---
+++ spec/features/restaurant_feature_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'Restaurant' do
+
+  context "no restaurants have been added" do
+    scenario "should display a link to add a restaurant" do
+      visit '/restaurants'
+      expect(page).to have_link 'Add Restaurant'
+    end
+  end
+
+  context "Can add a new restaurant" do
+    scenario "Prompts user to fill a form" do
+      visit '/restaurants/new'
+
+    end
+  end
+
+end
